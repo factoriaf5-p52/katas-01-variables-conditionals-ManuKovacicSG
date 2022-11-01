706a9a96e602630a1687adf0cd1b8752f94f7dec
Manu KSG <kovacicmanu@gmail.com>
2022-11-01 11:14:12 +0100

    finished all of the exercices :)

--- js/code.js
+++ js/code.js
@@ -1,20 +1,12 @@
 function evenOrOdd(n) {
-    if (n%2 ==0)
-    return "even";
-    else return "odd";
+  if (n % 2 == 0) return "even";
+  else return "odd";
 }
 
 /*greetings exercise*/
 function greeting(name) {
-    if (name ==="Javi")
-    return "Hola, Javi!";
-    else if
-    (name ==="gente")
-    return "Hola, gente!";
-    else (name ==="gente")
-    return "Hola, gente!";
+  if (name === "Javi") return "Hola, Javi!";
+  else if (name === "gente") return "Hola, gente!";
+  else (name === "Mary");
+  return "Hola, Mary!";
 }
-
-
-
-
